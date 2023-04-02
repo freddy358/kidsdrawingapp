a8b5e33b3cae03a281ff352cec8ba3b5f9e13c28
Farid Bakhishli <farid.bakhishli@gmail.com>
2023-04-03 02:38:16 +0400

    first commit

---
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.kidsdrawingapp
+
+import android.app.Dialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private var drawingView: DrawingView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        drawingView = findViewById(R.id.drawingView)
+        drawingView?.setSizeForBrush(20.toFloat())
+
+
+    }
+
+    private fun showBrushSizeChooserDialog(){
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size: ")
+        val smallBtn = brushDialog.ib_small_brush
+
+    }
+}
